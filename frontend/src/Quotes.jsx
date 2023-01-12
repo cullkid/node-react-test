0455c9c18c2aa1f9f6ffa9cdb2a175ddd2f0afe4
unknown <Eze Anthony>
2023-01-12 17:49:59 +0000

    initial commit

---
+++ frontend/src/Quotes.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Quotes() {
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  function getQuote() {
+    axios
+      .get("http://localhost:5000/", { crossdomain: true })
+      .then((response) => {
+        console.log(response.data);
+        setText(response.data.text);
+        setAuthor(response.data.author);
+      });
+  }
+
+  // function getJobs() {
+  //   axios
+  //     .get("http://localhost:4000/api/jobRoute", { crossdomain: true })
+  //     .then((response) => {
+  //       console.log(response.jobRoute);
+  //       // setText(response.data.text);
+  //       // setAuthor(response.data.author);
+  //     });
+  // }
+
+  return (
+    <div>
+      <button onClick={getQuote}>Generate Quote</button>
+      <h1>{text}</h1>
+      <h3>{"-" + author}</h3>
+    </div>
+  );
+}
+
+export default Quotes;
